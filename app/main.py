8e2307ba472566175685ff84722792cea674c20b
Julia Todorova <julkata@Julias-MBP-2.malibu.home>
2020-02-11 20:02:24 -0800

    Method set up

--- app/main.py
+++ app/main.py
@@ -8,56 +8,38 @@ from api import ping_response, start_response, move_response, end_response
 
 @bottle.route('/')
 def index():
-    return '''
-    Battlesnake documentation can be found at
-       <a href="https://docs.battlesnake.com">https://docs.battlesnake.com</a>.
-    '''
+    return ''
 
 
 @bottle.route('/static/<path:path>')
 def static(path):
-    """
-    Given a path, return the static file located relative
-    to the static folder.
-
-    This can be used to return the snake head URL in an API response.
-    """
     return bottle.static_file(path, root='static/')
 
 
 @bottle.post('/ping')
 def ping():
-    """
-    A keep-alive endpoint used to prevent cloud application platforms,
-    such as Heroku, from sleeping the application instance.
-    """
     return ping_response()
 
 
 @bottle.post('/start')
 def start():
     data = bottle.request.json
+    game_id = data['game']['id']
 
-    """
-    TODO: If you intend to have a stateful snake AI,
-            initialize your snake state here using the
-            request's data if necessary.
-    """
     print(json.dumps(data))
 
-    color = "#00FF00"
-
-    return start_response(color)
+    return {
+        "color": "#00b3b3",
+        "headType": "tongue",
+        "tailType": "curled",
+    }
 
 
 @bottle.post('/move')
 def move():
     data = bottle.request.json
+    turn = data['turn']
 
-    """
-    TODO: Using the data from the endpoint request object, your
-            snake AI must choose a direction to move in.
-    """
     print(json.dumps(data))
 
     directions = ['up', 'down', 'left', 'right']
@@ -65,22 +47,42 @@ def move():
 
     return move_response(direction)
 
-
 @bottle.post('/end')
 def end():
     data = bottle.request.json
 
-    """
-    TODO: If your snake AI was stateful,
-        clean up any stateful objects here.
-    """
     print(json.dumps(data))
 
     return end_response()
 
 
-# Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
+
+
+def game():
+    data = bottle.request.json
+
+
+def board():
+    data = bottle.request.json
+    height = data['board']['height']
+    width = data['board']['width']
+
+
+def snake():
+    data = bottle.request.json
+    snake_id = data['you']['id']
+    snake_name = data['you']['name']
+    snake_health = data['you']['health']
+    snake_body_x = data['body']['x']
+    snake_body_y = data['body']['y']
+
+
+def food_location():
+    data = bottle.request.json
+    x = data['board']['food']['x']
+    y = data['board']['food']['y']
+
 
 if __name__ == '__main__':
     bottle.run(
